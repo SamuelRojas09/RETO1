8b48e56e7690975ba9ac627d8c741ac042648e48
SamuelRojas09 <samrojas@unal.edu.co>
2026-04-16 19:28:19 -0500

    Update RETO1.1.py

--- RETO1.1.py
+++ RETO1.1.py
@@ -1,11 +1,3 @@
-"""
-Definí la función operaciones que recibe dos números y un operador. 
-Según el operador, realiza suma, resta, multiplicación o división (verificando que no sea división entre cero). 
-Si el operador es inválido, devuelve un mensaje de error. 
-Luego, con input(), pido los números y el operador al usuario,
-y uso try-except para manejar errores si ingresa algo que no sea un número.
-"""
-
 def operations(number_1: int, number_2: int, character: str) -> float | str:
 
     if character == "+":
